0ec1c2e20d9544e5133d3fe680e7656e79091bee
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-17 21:50:41 -0300

    ajustando alinhamento do header com o body

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header () {
     return(
-        <header className="px-20 py-5 border-b-1 border-[#f1f1f1] rounded-b-2xl">
+        <header className="px-28 py-5 border-b-1 border-[#f1f1f1] rounded-b-2xl">
             <h1 className="text-3xl font-bold">Countries</h1>
         </header>
     )
